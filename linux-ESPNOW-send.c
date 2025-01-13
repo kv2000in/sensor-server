781d9447fd2778a1c68e0d57ef7398e962ccb275
kv2000in <kv2000inn@gmail.com>
2025-01-12 22:49:38 -0500

    Passing the command via argument sudo ./mysend wlx00xxxxxx 0x43

--- linux-ESPNOW-send.c
+++ linux-ESPNOW-send.c
@@ -9,12 +9,11 @@
 #include <arpa/inet.h>
 #include <assert.h>
 
-
 static uint8_t gu8a_dest_mac[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 
 int create_raw_socket(char *dev)
 {
-	struct sockaddr_ll s_dest_addr; //code from sender
+	struct sockaddr_ll s_dest_addr; // code from sender
 	struct ifreq ifr;
 	int fd, ifi, rb;
 	
@@ -24,39 +23,44 @@ int create_raw_socket(char *dev)
 	(void)memset(&s_dest_addr, 0, sizeof(s_dest_addr));
 	
 	fd = socket(PF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
-	assert(fd != -1); //abort if error
+	assert(fd != -1); // abort if error
 	
 	strncpy((char *)ifr.ifr_name, dev, IFNAMSIZ);
 	ifi = ioctl(fd, SIOCGIFINDEX, &ifr);
-	assert(ifi != -1); //abort if error
+	assert(ifi != -1); // abort if error
 	
 	s_dest_addr.sll_family = PF_PACKET;
-		//we don't use a protocol above ethernet layer, just use anything here
 	s_dest_addr.sll_protocol = htons(ETH_P_ALL);
 	s_dest_addr.sll_ifindex = ifr.ifr_ifindex;
 	s_dest_addr.sll_hatype = ARPHRD_ETHER;
-	s_dest_addr.sll_pkttype = PACKET_OTHERHOST; //PACKET_OUTGOING
+	s_dest_addr.sll_pkttype = PACKET_OTHERHOST; // PACKET_OUTGOING
 	s_dest_addr.sll_halen = ETH_ALEN;
-		//MAC - begin
 	s_dest_addr.sll_addr[0] = gu8a_dest_mac[0];
 	s_dest_addr.sll_addr[1] = gu8a_dest_mac[1];
 	s_dest_addr.sll_addr[2] = gu8a_dest_mac[2];
 	s_dest_addr.sll_addr[3] = gu8a_dest_mac[3];
 	s_dest_addr.sll_addr[4] = gu8a_dest_mac[4];
 	s_dest_addr.sll_addr[5] = gu8a_dest_mac[5];
-		//MAC - end
-	s_dest_addr.sll_addr[6] = 0x00; //not used
-	s_dest_addr.sll_addr[7] = 0x00; //not used
+	s_dest_addr.sll_addr[6] = 0x00; // not used
+	s_dest_addr.sll_addr[7] = 0x00; // not used
 	
 	rb = bind(fd, (struct sockaddr *)&s_dest_addr, sizeof(s_dest_addr));
-	assert(rb != -1); //abort if error
+	assert(rb != -1); // abort if error
 	
 	return fd;
 }
 
 int main(int argc, char **argv)
 {
+	if (argc < 3)
+	{
+		fprintf(stderr, "Usage: %s <interface> <hex_byte>\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+	
 	char *dev = argv[1];
+	char *hex_byte_str = argv[2];
+	uint8_t additional_byte = (uint8_t)strtol(hex_byte_str, NULL, 16);
 	
 	int sock_fd = -1;
 	int32_t s32_res = -1;
@@ -69,37 +73,26 @@ int main(int argc, char **argv)
 		goto LABEL_CLEAN_EXIT;
 	}
 	
-		//printf("Socket created\n");
-	
 	fflush(stdout);
 	
 	sleep(1);
-	
-		//  printf("******Sending data using raw socket over  %s \n", dev);
-	
 	
 	uint8_t data[82] = {
 		0x00, 0x00, 0x26, 0x00, 0x2f, 0x40, 0x00, 0xa0, 0x20, 0x08, 0x00, 0xa0, 0x20, 0x08, 0x00, 0x00,
 		0xdf, 0x32, 0xfe, 0x1f, 0x00, 0x00, 0x00, 0x00, 0x10, 0x0c, 0x6c, 0x09, 0xc0, 0x00, 0xd3, 0x00,
 		0x00, 0x00, 0xd3, 0x00, 0xc7, 0x01, 0xd0, 0x00, 0x3a, 0x01, 0x58, 0xbf, 0x25, 0x82, 0x8e, 0xd8,
 		0x84, 0xcc, 0xa8, 0xa9, 0xe1, 0xe8, 0x58, 0xbf, 0x25, 0x82, 0x8e, 0xd8, 0x70, 0x51, 0x7f, 0x18,
-		0xfe, 0x34, 0xa2, 0x03, 0x92, 0xb0, 0xdd, 0x06, 0x18, 0xfe, 0x34, 0x04, 0x01, 0x43, 0x1c, 0xd5, 0x35, 0xd3}; //FROM TLINK TO ESP
+		0xfe, 0x34, 0xa2, 0x03, 0x92, 0xb0, 0xdd, 0x06, 0x18, 0xfe, 0x34, 0x04, 0x01,
+		additional_byte,0x1c, 0xd5, 0x35, 0xd3 
+	};
 	
-	s32_res = sendto(sock_fd,data,82,0,NULL,0);
+	s32_res = sendto(sock_fd, data, sizeof(data), 0, NULL, 0);
 	
 	if (-1 == s32_res)
 	{
 		perror("Socket send failed");
 		goto LABEL_CLEAN_EXIT;
 	}
-		// int i = 0;
-		// for (i = 0; i < 331; i++)
-		//     printf("%x ", data[i]);
-	
-		// printf("\n\n");
-	
-		// usleep(100);
-	
 	
 LABEL_CLEAN_EXIT:
 	if (sock_fd > 0)
@@ -107,7 +100,7 @@ LABEL_CLEAN_EXIT:
 		close(sock_fd);
 	}
 	
-	printf("***** sent\n");
+	printf("***** Packet sent with additional byte: 0x%02x *****\n", additional_byte);
 	
 	return EXIT_SUCCESS;
 }
