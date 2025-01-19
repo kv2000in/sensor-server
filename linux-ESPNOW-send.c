e27b86688d7cf499a3cfc2273647fa43eaa1a12a
kv2000in <kv2000inn@gmail.com>
2025-01-19 18:02:17 -0500

    dynamically set sender and destination MAC

--- linux-ESPNOW-send.c
+++ linux-ESPNOW-send.c
@@ -52,21 +52,31 @@ int create_raw_socket(char *dev)
 
 int main(int argc, char **argv)
 {
-	if (argc < 3)
+	if (argc < 4)
 	{
-		fprintf(stderr, "Usage: %s <interface> <hex_byte>\n", argv[0]);
+		fprintf(stderr, "Usage: %s <interface> <senderMACAddress> <destinationMACAddress> <additional_byte>\n", argv[0]);
 		return EXIT_FAILURE;
 	}
 	
 	char *dev = argv[1];
-	char *hex_byte_str = argv[2];
-	uint8_t additional_byte = (uint8_t)strtol(hex_byte_str, NULL, 16);
+	uint8_t senderMAC[6];
+	uint8_t destinationMAC[6];
+	uint8_t additional_byte;
+	
+		// Parse senderMACAddress (argv[2]) and destinationMACAddress (argv[3])
+	sscanf(argv[2], "\\x%02hhx\\x%02hhx\\x%02hhx\\x%02hhx\\x%02hhx\\x%02hhx", 
+		   &senderMAC[0], &senderMAC[1], &senderMAC[2], &senderMAC[3], &senderMAC[4], &senderMAC[5]);
+	
+	sscanf(argv[3], "\\x%02hhx\\x%02hhx\\x%02hhx\\x%02hhx\\x%02hhx\\x%02hhx", 
+		   &destinationMAC[0], &destinationMAC[1], &destinationMAC[2], &destinationMAC[3], &destinationMAC[4], &destinationMAC[5]);
+	
+		// Parse additional_byte (argv[4]) in the same format
+	sscanf(argv[4], "\\x%02hhx", &additional_byte);
 	
 	int sock_fd = -1;
 	int32_t s32_res = -1;
 	
 	sock_fd = create_raw_socket(dev); /* Creating the raw socket */
-	
 	if (-1 == sock_fd)
 	{
 		perror("Could not create the socket");
@@ -74,7 +84,6 @@ int main(int argc, char **argv)
 	}
 	
 	fflush(stdout);
-	
 	sleep(1);
 	
 	uint8_t data[82] = {
@@ -83,8 +92,15 @@ int main(int argc, char **argv)
 		0x00, 0x00, 0xd3, 0x00, 0xc7, 0x01, 0xd0, 0x00, 0x3a, 0x01, 0x58, 0xbf, 0x25, 0x82, 0x8e, 0xd8,
 		0x84, 0xcc, 0xa8, 0xa9, 0xe1, 0xe8, 0x58, 0xbf, 0x25, 0x82, 0x8e, 0xd8, 0x70, 0x51, 0x7f, 0x18,
 		0xfe, 0x34, 0xa2, 0x03, 0x92, 0xb0, 0xdd, 0x06, 0x18, 0xfe, 0x34, 0x04, 0x01,
-		additional_byte,0x1c, 0xd5, 0x35, 0xd3 
+		additional_byte, 0x1c, 0xd5, 0x35, 0xd3 
 	};
+	
+		// Replace data array values dynamically
+	for (int i = 0; i < 6; i++) {
+		data[42 + i] = destinationMAC[i]; // Replace destinationMAC in data[42] to data[47]
+		data[54 + i] = destinationMAC[i]; // Replace destinationMAC in data[54] to data[59]
+		data[48 + i] = senderMAC[i];      // Replace senderMAC in data[48] to data[53]
+	}
 	
 	s32_res = sendto(sock_fd, data, sizeof(data), 0, NULL, 0);
 	
@@ -100,7 +116,8 @@ LABEL_CLEAN_EXIT:
 		close(sock_fd);
 	}
 	
-	printf("***** Packet sent with additional byte: 0x%02x *****\n", additional_byte);
+	printf("***** Packet sent with byte: 0x%02x *****\n", additional_byte);
 	
 	return EXIT_SUCCESS;
 }
+
