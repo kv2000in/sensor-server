d39b23c394d3e8a22f38646df9db1ff9cb232461
kv2000in <kv2000inn@gmail.com>
2025-02-04 19:04:42 -0500

    disabling lcd functions

--- sensor-server.py
+++ sensor-server.py
@@ -2067,6 +2067,7 @@ if __name__ == '__main__':
 		killer = GracefulKiller()
 		while True:
 			try:
+			print("Python lcdticker dummy call")
 			#lcdticker() #March 2022 -Getting I/O error - disable LCD
 			except Exception as e:
 				print(e)
