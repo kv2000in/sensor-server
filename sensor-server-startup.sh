db012c669a546d20c08ee1781e74000891f5757f
kv2000in <kv2000inn@gmail.com>
2026-01-10 18:25:45 -0500

    fixing startup file. trying to catch what causes random reboots. this startup script should redirect errors to journal

--- sensor-server-startup.sh
+++ sensor-server-startup.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# redirect stdout/stderr to a file
-exec >sensor-server-startuplogfile.txt 2>&1
+set -x
 cd /home/pi/Downloads/playground/sensor-server/ || exit 1
-sleep 1
 /usr/bin/python /home/pi/Downloads/playground/sensor-server/sensor-server.py
-
-
